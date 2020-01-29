9a88ac10c9bc7a29b58f81a2b5fbc11499bcd398
carlos roberto cardoso dos reis bueno <ccarlosbueno@outlook.com>
2020-01-29 17:12:50 -0300

    Create Index

--- PocApi/Infra/AcessoBaseDados/AcessoMongo/ContextoMongo.cs
+++ PocApi/Infra/AcessoBaseDados/AcessoMongo/ContextoMongo.cs
@@ -12,16 +12,18 @@ namespace Infra.AcessoBaseDados.AcessoMongo
 		private IMongoDatabase _dbMongo { get; set; }
 		public ContextoMongo()
 		{
-			
-				_mongoCliente = new MongoClient("mongodb://pocstone:pocstone@mongodb_server:27017");
-				_dbMongo = _mongoCliente.GetDatabase("PocStone");
 
-				if (!new GerenciadorDeColecoes().ColecaoExiste(_mongoCliente, "PocStone", "Pagamentos"))
-				{
-					_dbMongo.CreateCollection("Pagamentos");
-				}
-		
-		
+			_mongoCliente = new MongoClient("mongodb://pocstone:pocstone@mongodb_server:27017");
+			_dbMongo = _mongoCliente.GetDatabase("PocStone");
+
+			if (!new GerenciadorDeColecoes().ColecaoExiste(_mongoCliente, "PocStone", "Pagamentos"))
+			{
+				_dbMongo.CreateCollection("Pagamentos");
+				var collection = _dbMongo.GetCollection<Pagamentos>("Pagamentos");
+				collection.Indexes.CreateOne(Builders<Pagamentos>.IndexKeys.Ascending(_ => _.codPagamento));
+			}
+
+
 		}
 
 		public IMongoCollection<Pagamentos> Pagamentos
